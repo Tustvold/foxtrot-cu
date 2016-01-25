05cb3b07865875ea6e4fe09dcd2ab76e167d1bcc
Raphael Taylor-Davies <r.taylordavies@googlemail.com>
2016-01-25 12:46:45 +0000

    Basic Gradle Project setup with JUnit 4 for testing

---
+++ src/main/cam.ac.uk.foxtrot/Main.java
@@ -0,0 +1,8 @@
+package cam.ac.uk.foxtrot;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/test/cam.ac.uk.foxtrot/MainTest.java
@@ -0,0 +1,14 @@
+package cam.ac.uk.foxtrot;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void testMain() throws Exception {
+        Assert.assertTrue(true);
+    }
+}
