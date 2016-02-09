91d6253098834ad19c54f1e2cd894a97f5638765
Curdled <joe.isaacs@live.co.uk>
2016-02-09 17:00:43 +0000

    added the index page with two views using BlockListRenderer and ObjFileRenderer

--- foxtrot-cu-server/routes/upload.js
+++ foxtrot-cu-server/routes/upload.js
@@ -6,23 +6,34 @@ var router = express.Router();
 var upload = multer({ dest: '/tmp/uploads/' });
 
 
-router.post('/', upload.any(), function(req,res,next){
+router.post('/', upload.single('uploaded-mesh'), function(req,res,next){
 
-    child = exec('java -jar jar/backend.jar ' + req.files[0].path);
-    console.log('here')
-    console.log(child)
+    child = exec('java -jar jar/backend.jar ' + req.file.path);
+    console.log(req.file)
+
+    // Any data from the jar
     child.stdout.on('data', function(data) {
-        res.write(data);
+        if(!res.finished) {
+            res.write(data);
+        }
     });
+
+    // Any error will return those errors
     child.stderr.on('data', function(data) {
         console.log(data)
-    })
-    child.on('exit', function (code) {
-        setTimeout(function() { // to simulate a long running jar file.
-            res.end(code.toString());
-            console.log('child process exited with code ' + code);
-        }, 5 * 1000)
+        res.status(200).send({ error: data }).end()
     });
+
+    // If the jar finished successfully
+    child.on('exit', function (code) {
+        if(!res.finished) {
+            setTimeout(function () { // to simulate a long running jar file.
+                res.end(code.toString());
+                console.log('child process exited with code ' + code);
+            }, 5 * 1000)
+        }
+    });
+
 });
 
 module.exports = router;
