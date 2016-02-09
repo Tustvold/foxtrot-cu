afeb9552d0d1a575a947f7cf57844589b31a8564
Raphael Taylor-Davies <r.taylordavies@googlemail.com>
2016-02-09 13:17:06 +0000

    Added interfaces for BlockListRenderer and ObjFileRenderer

---
+++ foxtrot-cu-server/public/scripts/BlockListRenderer.js
@@ -0,0 +1,12 @@
+
+BlockListRenderer = function(width, height, domElement) {
+    // Will create a canvas of the specified dimensions added as a child of domElement
+
+    this.setBlockList = function(blockList) {
+        // Will set this element to render the provided blocklist
+    }
+
+    this.onBlockSelected = function(block) {
+
+    }
+}
---
+++ foxtrot-cu-server/public/scripts/ObjFileRenderer.js
@@ -0,0 +1,5 @@
+ObjFileRenderer = function(objFileURL, width, height, domElement) {
+    // Will render the provided objFile in a canvas of the specified dimensions added as a child of domElement
+    // I believe objFileURL can be a local URL (e.g file://) but this might not be possible - assume it can be for now
+
+}
