9da7a3f185bdb185c0967bd52f94da290a17cf4c
isobelcambridge <isobelcambridge@gmail.com>
2016-02-11 00:27:45 +0000

    Updated IntersectionRemover to give 2d Point3f array of polygons
    (Also uses Geometry not MultiPolygon)

--- backend/src/main/cam/ac/uk/foxtrot/voxelisation/IntersectionRemover.java
+++ backend/src/main/cam/ac/uk/foxtrot/voxelisation/IntersectionRemover.java
@@ -1,15 +1,16 @@
-import com.sun.j3d.utils.geometry.GeometryInfo;
 import com.vividsolutions.jts.geom.*;
 import com.vividsolutions.jts.operation.union.UnaryUnionOp;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
-import javax.media.j3d.TriangleArray;
 import javax.vecmath.Point3f;
 
 public class IntersectionRemover {
-    MultiPolygon union;
+    Geometry union;
+    Point3f[][] polygonArray;
 
     public IntersectionRemover(Point3f[] originalCoordinates){
         List<Geometry> geometryList = new ArrayList<>();
@@ -21,18 +22,63 @@ public class IntersectionRemover {
             Coordinate[] coordinates = {coordinate1,coordinate2,coordinate3,coordinate1}; //coordinates of triangle
             geometryList.add(factory.createPolygon(factory.createLinearRing(coordinates),null)); //add triangle to list of geometries
         }
-        union = (MultiPolygon)UnaryUnionOp.union(geometryList); //merge geometries, overlapping triangles merged
+        union = UnaryUnionOp.union(geometryList); //merge geometries, overlapping triangles merged
+        polygonArray = toPoint2dArray(union);
     }
 
     // convert original J3D Point3f to JTS Coordinates
     private Coordinate toJTSCoordinate(Point3f point) {
-        float[] xyz = new float[3];
-        point.get(xyz);
-        return new Coordinate(xyz[0],xyz[1],xyz[2]);
+        return new Coordinate(point.x,point.y,point.z);
     }
 
-    public MultiPolygon getResult() {
+    // convert JTS Coordinates to J3D Point3f
+    private Point3f toJ3DPoint3f(Coordinate coordinate) {
+        return new Point3f((float)coordinate.x,(float)coordinate.y,0);
+    }
+
+    // convert Geometry to array of arrays of Point3fs
+    private Point3f[][] toPoint2dArray(Geometry geometry) {
+        Coordinate[] coordinates = geometry.getCoordinates();
+        LinkedList<Point3f[]> polygonList = new LinkedList<>();
+        LinkedList<Point3f> currentPolygon = new LinkedList<>();
+        Point3f currentStart = null;
+        int length = coordinates.length;
+        if(length>0) {
+            currentStart = toJ3DPoint3f(coordinates[0]);
+            currentPolygon.add(currentStart);
+        }
+        for(int i = 1; i<length; i++) {
+            Point3f point = toJ3DPoint3f(coordinates[i]);
+            if (point.equals(currentStart)) {
+                polygonList.add(currentPolygon.toArray(new Point3f[0]));
+                currentPolygon = new LinkedList<>();
+                if (i < length - 1) {
+                    currentStart = toJ3DPoint3f(coordinates[++i]);
+                    currentPolygon.add(currentStart);
+                }
+            }
+            else {
+                currentPolygon.add(point);
+            }
+        }
+        return polygonList.toArray(new Point3f[0][]);
+    }
+
+    public Geometry getGeometry() {
         return union;
+    }
+
+    public Point3f[][] getPolygonArray() {
+        return polygonArray;
+    }
+
+    public static void main(String[] args) {
+        Point3f[] points = {new Point3f(0,0,0), new Point3f(2,0,0), new Point3f(1,2,0),
+                            new Point3f(1,0,0), new Point3f(3,0,0), new Point3f(2,2,0),
+                            new Point3f(0.5f,1.5f,0), new Point3f(2.5f,1.5f,0), new Point3f(1.5f,3.5f,0)};
+        IntersectionRemover merged = new IntersectionRemover(points);
+        System.out.println(Arrays.toString(merged.getGeometry().getCoordinates()));
+        System.out.println(Arrays.deepToString(merged.getPolygonArray()));
     }
 
 }
