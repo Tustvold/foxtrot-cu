20021b0a6504bc51468e5ea66c7ceab611348964
Curdled <joe.isaacs@live.co.uk>
2016-02-22 10:05:43 +0000

    fixed the serializer

--- backend/src/main/cam/ac/uk/foxtrot/serializer/BlockJSONSerializer.java
+++ backend/src/main/cam/ac/uk/foxtrot/serializer/BlockJSONSerializer.java
@@ -1,10 +1,7 @@
 package cam.ac.uk.foxtrot.serializer;
 
 import cam.ac.uk.foxtrot.voxelisation.Block;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
+import com.google.gson.*;
 
 import java.lang.reflect.Type;
 
@@ -16,7 +13,7 @@ public class BlockJSONSerializer implements JsonSerializer<Block> {
     @Override
     public JsonElement serialize(Block block, Type typeOfSrc, JsonSerializationContext context) {
         JsonObject object = new JsonObject();
-        object.addProperty("custom_part_array", String.valueOf(block.getCustomPart()));
+        object.addProperty("custom_part_array", new Gson().toJson(block.getCustomPart()));
         object.addProperty("use_custom_part", block.isCustom());
         object.addProperty("custom_part_index", block.getCustomPartIndex());
 
